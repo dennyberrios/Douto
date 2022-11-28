0ae73e27dc19fe7068d0e86e929b226403e1bebe
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-28 13:33:07 -0300

    Create Doacoes.jsx

---
+++ SPRINT_03/frontEnd/DOUTO/src/Pages/Doacoes/Doacoes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Doacoes.css'
+
+export default function Doacoes() {
+  return (
+    <main className='mainDoacoes'> 
+        <section className='sectionDoacoes'>
+            <h1>DOAÇÕES</h1>
+        </section>
+        <section className='sectionDoacoes'>
+            <h3>O Douto é uma plataforma de educação infantil sem fins lucrativos. O Douto
+                precisa da sua ajuda para levar o prazer da leitura as crianças mais vulneráveis da sociedade. Para
+                doar faça um pix de qualquer valor para a chave xxxxxxxxxx. Seja um parceiro Douto!</h3>
+        </section>
+    </main>
+  )
+}
